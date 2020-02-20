2a38ad1580601d4c3124b905dd6a968c812bc996
unknown <eric.pandolfo@gmail.com>
2020-02-20 13:38:41 -0300

    primeiro commit

---
+++ src/main/java/com/example/api/calculadora/CalculadoraApiApplication.java
@@ -0,0 +1,14 @@
+package com.example.api.calculadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class CalculadoraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/api/calculadora/controller/GerenciadorChamadas.java
@@ -0,0 +1,55 @@
+package com.example.api.calculadora.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/calculadora")
+public class GerenciadorChamadas {
+
+
+
+    @GetMapping(path = "/somar/{num1}/{num2}")
+    public ResponseEntity<Integer> somar(@PathVariable("num1") Integer textA, @PathVariable("num2") Integer textB) {
+        System.out.println("\nChamada de Soma recebida");
+        Integer resultado = textA + textB;
+        return ResponseEntity.status(HttpStatus.OK).body(resultado);
+    }
+
+    @PostMapping(path = "/cargas")
+    public ResponseEntity<String> cargas(@Valid @RequestBody String bodyRequest) {
+        System.out.printf("\nChamada POST de cargas recebida" + "\nBody:\n");
+        System.out.println(bodyRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(bodyRequest);
+    }
+
+    @GetMapping(path = "/get")
+    public ResponseEntity<String> listar() {
+        System.out.printf("\nChamada GET de vagões disponíveis recebida");
+        String retorno = "[{\n" +
+                "\t\"codigo\": \"codigo\",\n" +
+                "\t\"peso\": \"peso\",\n" +
+                "\t\"produto\": \"produto\",\n" +
+                "\t\"data\": \"data\",\n" +
+                "\t\"densidade\": \"densidade\"\n" +
+                "}, {\n" +
+                "\t\"codigo\": \"codigo\",\n" +
+                "\t\"peso\": \"peso\",\n" +
+                "\t\"produto\": \"produto\",\n" +
+                "\t\"data\": \"data\",\n" +
+                "\t\"densidade\": \"densidade\"\n" +
+                "}, {\n" +
+                "\t\"codigo\": \"codigo\",\n" +
+                "\t\"peso\": \"peso\",\n" +
+                "\t\"produto\": \"produto\",\n" +
+                "\t\"data\": \"data\",\n" +
+                "\t\"densidade\": \"densidade\"\n" +
+                "}]";
+        return ResponseEntity.status(HttpStatus.OK).body(retorno);
+    }
+
+}
